5d2bc7b569c1ab938ebacae2e9e752f41654ee96
Daniel Berigoi <daniel.berigoi@jisc.ac.uk>
2025-10-22 13:02:47 +0100

    OC-1105 - text in separate component

---
+++ ui/src/components/PublicationBundle/PublicationBundleInstructions.tsx
@@ -0,0 +1,11 @@
+const PublicationBundleInstructions = (): JSX.Element => {
+    return (
+        <p className="xl:w-2/3 2xl:w-1/2 text-sm text-grey-700 transition-colors duration-500 dark:text-grey-100 mb-8">
+            The publication bundles feature allows multiple publications to be collated into a single entity. Once
+            you've created a bundle, you can share the link with others to view it, or export it to other systems such
+            as journals or peer review platforms like PCI registered reports.
+        </p>
+    );
+};
+
+export default PublicationBundleInstructions;
