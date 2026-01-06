42386e0f7ccb9edd181caa0efa8f4e8155ac46c5
Daniel Berigoi <daniel.berigoi@jisc.ac.uk>
2026-01-06 13:20:20 +0000

    update dependencies in package-lock.json to latest versions

--- api/src/components/oai/response.ts
+++ api/src/components/oai/response.ts
@@ -313,7 +313,7 @@ export const generateResumptionToken = (params: {
         ...(params.until && { until: params.until })
     };
 
-    return Buffer.from(JSON.stringify(tokenData)).toString('base64');
+    return Buffer.from(JSON.stringify(tokenData)).toString('base64url');
 };
 
 export const parseResumptionToken = (
